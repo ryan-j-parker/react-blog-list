9e056d730adf8727ca0d62cd596d047e9dc58f28
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-09-19 12:09:30 -0700

    add fetch function for blog posts

--- src/components/BlogCard/BlogCard.js
+++ src/components/BlogCard/BlogCard.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './BlogCard.css';
+import getBlogs from '../../services/client.js';
+import { useState, useEffect } from 'react';
 
 export default function BlogCard({ title, subtitle, text, image }) {
+  const [posts, setPosts] = useState([]);
+  
   return (
     <div className="blog-card">
       <div className="details">
